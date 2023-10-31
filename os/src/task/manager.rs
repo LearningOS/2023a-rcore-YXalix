e64f19b3317edd11616a9a47b9c3f4d6563be470
YXalix <516875454@qq.com>
2023-10-31 19:19:04 +0800

    补充丢失commit的stride调度

--- os/src/task/manager.rs
+++ os/src/task/manager.rs
@@ -23,7 +23,26 @@ impl TaskManager {
     }
     /// Take a process out of the ready queue
     pub fn fetch(&mut self) -> Option<Arc<TaskControlBlock>> {
-        self.ready_queue.pop_front()
+        let mut task: Option<Arc<TaskControlBlock>> = None;
+        let len = self.ready_queue.len();
+        for _ in 0..len {
+            let task_new = self.ready_queue.pop_front().unwrap();
+            if let Some(ref task_i) = task {
+                if task_i.inner_exclusive_access().pass > task_new.inner_exclusive_access().pass {
+                    self.ready_queue.push_back(task_i.clone());
+                    task = Some(task_new);
+                } else {
+                    self.ready_queue.push_back(task_new);
+                }
+            } else {
+                task = Some(task_new);
+            }
+        }
+        if let Some(ref task) = task {
+            let mut inner = task.inner_exclusive_access();
+            inner.pass += inner.stride;
+        }
+        task
     }
 }
 
